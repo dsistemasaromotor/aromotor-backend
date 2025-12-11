13579617d14adf5d2f739b22011f13af5040582d
Steeven Maila <steevenandresmaila@gmail.com>
2025-12-11 10:50:11 -0500

    Actualización para subida correcion de frontend

--- estadoCuenta/src/App.jsx
+++ estadoCuenta/src/App.jsx
@@ -14,7 +14,7 @@ import EditarUsuario from './pages/EditarUsuario.jsx'
 function App() {
   return (
 
-    <BrowserRouter>
+    <BrowserRouter basename='/estadoCuenta'>
       <MainWrapper>
         <Routes>
           <Route path="/estadoCuenta/login" element={<Login/>} />

2ce1b180d6f0564ae99d0061fd535cfe08021ecb
Steeven Maila <steevenandresmaila@gmail.com>
2026-01-02 09:53:17 -0500

    Se corrige el backend test 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i3vpurbg7f98%z-afot!m9q_$t*mxxf&oh1i8kyk*_#p@@695n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'estadocuenta-2f2j.onrender.com', 'aromotor-backend.onrender.com', 'steven-2026-backend.c9r1sk.easypanel.host', '148.230.80.238']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'estadocuenta-2f2j.onrender.com', 'aromotor-backend.onrender.com', 'steven_2026_backend', '148.230.80.238']
 
 
 # Application definition
